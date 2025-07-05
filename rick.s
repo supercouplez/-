17e7e8a8f13c67b1a7e19d96f26838446fd755e5
supercouplez <bil33qiss@gmail.com>
2025-07-06 03:04:14 +0700

    Update rick.s

--- rick.s
+++ rick.s
@@ -6,21 +6,23 @@ input_buffer: resb 4
 
 section .text
 ascii_to_input:
-    mov     eax, 0          
-    mov     edi, 0          
-    mov     rsi, input_buffer
-    mov     edx, 4          
+    mov rax, 0          
+    mov rdi, 0          
+    mov rsi, input_buffer
+    mov rdx, 4
+    syscall
 
-    xor     rcx, rcx
+    xor rcx, rcx
 .convert_loop:
-    cmp     rcx, 4
-    jge     .done
+    cmp rcx, 4
+    jge .done
 
-    movzx   eax, byte [input_buffer + rcx]
-    cvtsi2ss xmm0, eax
-    movss   [input + rcx*4], xmm0
-    inc     rcx
-    jmp     .convert_loop
+    movzx eax, byte [input_buffer + rcx] 
+    sub eax, 65                  
+    cvtsi2ss xmm0, eax                   
+    movss [input + rcx*4], xmm0          
+    inc rcx
+    jmp .convert_loop
 
 .done:
     ret
