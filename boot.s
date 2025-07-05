352a86ca8ad3b1cbad09e13c0d8e44766d903135
supercouplez <bil33qiss@gmail.com>
2025-07-06 02:17:17 +0700

    Update boot.s

--- boot.s
+++ boot.s
@@ -1,10 +1,12 @@
 global _start
-extern forward_pass        
+extern forward_pass
+extern softmax_pass
 
 section .text
 _start:
     call forward_pass
+    call softmax_pass
 
-    mov rax, 60         
-    xor rdi, rdi        
+    mov rax, 60
+    xor rdi, rdi
     syscall
