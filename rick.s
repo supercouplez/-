e2b40e17503ef92afe49a3a4a81f62321d271d22
supercouplez <bil33qiss@gmail.com>
2025-07-06 03:00:27 +0700

    Update rick.s

--- rick.s
+++ rick.s
@@ -1,27 +1,26 @@
 global ascii_to_input
 extern input
 
+section .bss
+input_buffer: resb 4
+
 section .text
 ascii_to_input:
-    
-    mov     rax, 0          
-    mov     rdi, 0          
+    mov     eax, 0          
+    mov     edi, 0          
     mov     rsi, input_buffer
-    mov     rdx, 4          
+    mov     edx, 4          
 
     xor     rcx, rcx
 .convert_loop:
     cmp     rcx, 4
     jge     .done
 
-    movzx   eax, byte [input_buffer + rcx] 
-    cvtsi2ss xmm0, eax                   
-    movss   [input + rcx*4], xmm0          
+    movzx   eax, byte [input_buffer + rcx]
+    cvtsi2ss xmm0, eax
+    movss   [input + rcx*4], xmm0
     inc     rcx
     jmp     .convert_loop
 
 .done:
     ret
-
-section .bss
-input_buffer: resb 4
