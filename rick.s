e42e3cfc08b548fce29478e3f841318db61b3820
supercouplez <bil33qiss@gmail.com>
2025-07-06 03:27:50 +0700

    Update rick.s

--- rick.s
+++ rick.s
@@ -19,23 +19,11 @@ ascii_to_input:
     jge .done
 
     movzx eax, byte [input_buffer + rcx] 
-    sub eax, 65               
-    cvtsi2ss xmm0, eax
-    movss [input + rcx*4], xmm0
+    sub eax, 65                          
+    cvtsi2ss xmm0, eax                   
+    movss [input + rcx*4], xmm0         
     inc rcx
     jmp .convert_loop
 
 .done:
-    ; print input[0]
-    mov rax, 1         ; syscall write
-    mov rdi, 1         ; stdout
-    lea rsi, [input]   ; pointer ke input[0]
-    mov rdx, 4         ; 1 float (4 byte)
-    syscall
-
-mov rax, 1
-mov rdi, 1
-lea rsi, [input]
-mov rdx, 16
-syscall
     ret
